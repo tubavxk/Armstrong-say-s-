5340f57227b761ac0ef81a4b6d90036a73a1caef
Tuba <tubaazsoy@gmail.com>
2024-11-20 22:50:07 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Bir sayı girin: ");
+int sayi = Convert.ToInt32(Console.ReadLine());
+
+int toplam = 0;
+int sayı1 = sayi;
+
+if (toplam == sayı1)
+{
+    Console.WriteLine(" bir Armstrong sayısıdır.");
+}
+else
+{
+    Console.WriteLine("bir Armstrong sayısı değildir.");
+}
+
+Console.ReadKey();
